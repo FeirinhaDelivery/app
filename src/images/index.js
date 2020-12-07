59339ccad3d58a4ff5419b302777227b8bcf4a53
Leandro Henriques <lehenriques@gmail.com>
2020-12-07 16:55:43 -0300

    Adicionando as imagens do manifesto

--- src/images/index.js
+++ src/images/index.js
@@ -3,6 +3,11 @@ const images = {
     logo: require('./logo.png'),
     teste: require('./logo512.png'),
   },
+  manifest: {
+    muitos: require('./manifesto-mobile.833eee1d.png'),
+    assinaturas: require('./banner-assine-mobile.58dd3ce4.png'),
+    comunidade: require('./bg-whatsapp-mobile.33d2976f.png'),
+  },
 };
 
 export default images;
