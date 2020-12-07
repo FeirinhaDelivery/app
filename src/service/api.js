1928855afb44efbcc586d98362c5205bd379b7a7
Leandro Henriques <lehenriques@gmail.com>
2020-12-07 16:52:51 -0300

    Adicionando axios ao projeto

---
+++ src/service/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://app.ecwid.com/api/v3/28820124/',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  params: {
+    token: 'public_U8xi9fM3mEzGzY3Nh9YJduEDXPn9PVbs',
+  },
+});
+
+export default api;
