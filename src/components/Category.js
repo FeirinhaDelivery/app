126df0a26c567e3769b67a9ad92702c6de79c35f
Leandro Henriques <lehenriques@gmail.com>
2020-12-07 16:55:20 -0300

    Removendo importacao não usada

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, Image, Text, TouchableOpacity} from 'react-native';
 
 import api from '../service/api';
