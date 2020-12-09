03bfc80280635cee1ce1d47a00204223f670cd6f
Leandro Henriques <lehenriques@gmail.com>
2020-12-09 11:56:39 -0300

    atualizando subscription

--- src/pages/Subscriptions/index.js
+++ src/pages/Subscriptions/index.js
@@ -1,28 +1,71 @@
 import React from 'react';
-import {View, Button, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Button,
+  Text,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+
 import Footer from '../../components/Footer';
+import Images from '../../images';
 
 const Subscriptions = ({navigation}) => {
   return (
     <>
-      <View style={styles.center}>
-        <Text>Página de Assinaturas</Text>
-        <Button
-          title="Voltar para home"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
-      <Footer />
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <View>
+            <Image
+              style={styles.image}
+              source={Images.subscriptions.subscriptions}
+            />
+            <Text style={styles.text}>
+              Frete grátis com valor a partir de R$ 25,00 em compra mínima.
+            </Text>
+            <Text style={styles.textDisponivel}>
+              Disponível em Foz do Iguaçu
+            </Text>
+            <View style={styles.viewButton}>
+              <Button
+                title="Assine Agora!"
+                color="#25D366"
+                onPress={() => navigation.navigate('Home')}
+              />
+            </View>
+          </View>
+          <Footer />
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  textDisponivel: {
+    marginBottom: 7,
+    fontWeight: '600',
     textAlign: 'center',
+    fontSize: 17,
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: '600',
+    marginBottom: 10,
+    paddingHorizontal: 30,
+  },
+  image: {
+    width: '100%',
+    resizeMode: 'contain',
+    maxHeight: 350,
+    marginTop: -20,
   },
 });
 
